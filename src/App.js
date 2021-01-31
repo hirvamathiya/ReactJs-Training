d13772532b6796e4baab85c718d4f064d7d13c40
hirvamathiya <hirvamathiya@gmail.com>
2021-01-31 21:53:00 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Todolist from './Todolist';
+const App =()=>
+{
+    
+    const [inputList, setInputList] = useState("");
+    const [items, setItems] =useState([]);
+    const itemEvent=(event)=>{
+        setInputList(event.target.value);
+    };
+    
+    const listOfItems = () =>{
+         setItems((oldItems) =>{
+           return [...oldItems,inputList];
+         });
+         
+         setInputList('');
+      
+    };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const deleteItems = (id) =>{
+      
+      setItems((oldItems) =>{
+          return oldItems.filter((arrElem,index)=>{
+              return index!==id;
+          })
+      })
+  };
+
+    return(
+      <>
+      <div className="app">
+        <div className="centerDiv">
+           <br/>
+
+           <h1>Todo List</h1>
+           <br/>
+           <input type="text" placeholder="Add your todo here"  value={inputList} onChange={itemEvent}/>
+           <button onClick={listOfItems}>+</button>
+         
+          <ol>
+            {
+              items.map((itemval,index)=>{
+                return <Todolist key={index} id={index} text={itemval}  onSelect={deleteItems}    />;
+              })
+            }
+          </ol>
+           <div>
+               
+          </div> 
+        </div>
+      </div>
+      </>
+    );
+  
+
+
+};
 
 export default App;
